bb7b19d87f5d8aad6fbf8caab9845f261d5ee261
GHOST-Aram <owinoojala@gmail.com>
2023-07-20 09:20:25 +0300

    Chore: Practice mongoose by wds

--- User.js
+++ User.js
@@ -1,7 +1,70 @@
 import mongoose from 'mongoose'
 
-const userschema =  mongoose.Schema({name: String, nationality: String})
+const userschema =  mongoose.Schema({
+    name: {
+        type: String,
+    uppercase: true,
+    }, 
+    nationality: String, 
+    age: {
+        type: Number,
+        min: 10, 
+        max: 67,
+        validate:{
+            validator: v => v % 2 === 0,
+            message: props => `${props.value} is not an even number`
+        }
+    },
+    email: {
+        type: String,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    bestFriend: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User'
+    },
+    hobbies: [String],
+    address: {
+        street: String,
+        city: String
+    }
+})
 
+userschema.methods.sayHi = function(){
+    return `Hi ${this.name}`
+}
+
+userschema.statics.findByName = function(name){
+    return this.where({name: new RegExp(name, 'i')})
+}
+
+userschema.query.byName = function(name){
+    return this.where({name: new RegExp(name, 'i')})
+}
+
+userschema.virtual('named_email').get(function(){
+    return `${this.name}: ${this.email}`
+})
+
+userschema.pre('save', function(next){
+    this.updatedAt = Date.now()
+    next()
+})
+
+userschema.post('save', function(doc, next) {
+    doc.sayHi()
+    console.log('DOC SAYS hI')
+    next
+})
 const User = mongoose.model('User', userschema)
 
 export {User}
--- script.js
+++ script.js
@@ -1,10 +1,79 @@
+import mongoose from "mongoose";
 import { User } from "./User.js";
 
-const player = new User({ name: 'James', nationality: 'American'})
 
-const save = async(collection) =>{
-    await player.save()
-    console.log(player)
+const connect = async(uri) =>{
+     await mongoose.connect(uri)
+    console.log('Connected')
+
+}
+const save = async(doc) =>{
+    await doc.save()
+    // console.log(doc, 'saved!')
 }
 
-save(player)
+const create = async(model, docObject) =>{
+    const newDoc =    await model.create(docObject)
+    // console.log(newDoc, 'created')
+    return newDoc
+}
+
+connect('mongodb://localhost/testdb')
+const player = new User({name: 'Jane', nationality: 'Kenyan'})
+// save(player)
+const newUser = create(User, {name: 'Jane', nationality: 'South African'})
+// console.log(newUser.then(result => {console.log(result); save(result)}))
+
+create(User, {
+    name:'john',
+    nationality: 'American',
+    age: 42,
+    email: 'KINGDAO@gmail.com',
+    hobbies: ['Running', 'Reading', 'watching'],
+    bestFriend: "64b8c6f191666e143f78fd95",
+    address: {
+        street: '234 North Street',
+        city: 'New York'
+    }
+
+}).then(doc =>{
+    // console.log(doc)
+    save(doc)
+    console.log(doc.sayHi())
+}).catch((error) => console.error(error))
+
+User.findById("64b8b509d38ceb670e4f5e86").then(
+    user => console.log('Found user: ')
+)
+
+// User.find().then(users => console.log(users))
+
+User.findOne({ name: 'JOHN'}).then(p => {
+    // p.populate('bestFriend')
+    // console.log('populated', p)
+})
+User.where('name')
+    .equals('JOHN')
+    .limit(2).select('name')
+    .select('nationality')
+    .populate('bestFriend')
+    
+    .then(users =>{
+        
+        console.log(users)
+    })
+// User.deleteMany({ name: 'JOHN'}).then(
+//     result =>{
+//         // User.count({name: 'JOHN'}).then(count => console.log(count))
+//     }
+    
+// )
+
+User.findByName('John').limit(2).then(john => console.log(john))
+
+User.find().byName('jane').limit(3).then(janes => console.log(janes))
+
+User.findOne({name: 'JOHN'}).then(user => {
+    console.log(user)
+    console.log(user.named_email)
+})
